aa9380158160a03958339f447b8c29279582da04
Premasagar Rose <p@dharmafly.com>
2013-01-17 20:35:59 +0000

    Streamline code

--- jasonpeewee.js
+++ jasonpeewee.js
@@ -67,96 +67,65 @@
         return encodeURIComponent(key) + '=' + encodeURIComponent(value);
     }
 
-    // Accepts a params object and a boolean flag for whether the params should
-    // be alphanumerically sorted. Returns a URI-encoded query string
+    // Accepts a params object, which is first alphanumerically sorted.
+    // Returns a URI-encoded query string.
     // Does not include a '?' prefix, to allow concatenation of multiple strings
-    function encodeURLQueryString(params, sort){
-        var queryString = '',
-            keys, key, i, len;
+    function encodeAndSortQueryString(params){
+        var keys = sortedObjectKeys(params),
+            len = keys.length,
+            i = 0,
+            queryString = '';
 
-        if (sort === true){
-            keys = sortedObjectKeys(params);
-            for (i=0, len=keys.length; i<len; i++){
-                key = keys[i];
-
-                if (i){
-                    queryString += '&';
-                }
-                queryString += encodeParameter(params, key);
-            }
+        for (; i<len; i++){
+            queryString += '&' + encodeParameter(params, keys[i]);
         }
 
-        else {
-            for (key in params){
-                if (params.hasOwnProperty(key)){
-                    queryString += encodeParameter(params, key);
-                }
-            }
-        }
-
-        return queryString;
+        // Remove first '&' and return
+        return queryString.slice(1);
     }
 
     // Create global master callback and collection of private callbacks
-    function createCallbackCollection(callbackName){
+    function createCollection(callbackName){
         // Create array of private callbacks to the url
         var callbacks = privateCallbacks[callbackName] = [];
 
         // Create global master callback, which receives data from the remote API
         // and passes that data on to the private callbacks
         masterCallbacks[callbackName] = function(data){
-            var len = callbacks.length;
+            var len = callbacks.length,
+                i = 0;
 
             // Call all callbacks with the data
-            // We use a `for` loop, not a `while` loop, in case the callbacks
-            // create new callbacks
-            for (; len; len--){
-                // Remove the first callback in the array, and pass data to it
-                callbacks.shift()(data);
+            for (; i<len; i++){
+                callbacks[i](data);
             }
-
-            // Free up memory by deleting container
-            removeCallbackCollectionIfEmpty(callbackName);
         };
         
         return callbacks;
     }
 
-    function removeCallbackCollectionIfEmpty(callbackName){
-        var callbacks = privateCallbacks[callbackName];
-
-        // Only remove if there are no private callbacks remaining
-        if (callbacks && !callbacks.length){
-            delete privateCallbacks[callbackName];
-            delete masterCallbacks[callbackName];
-        }
+    function removeCollection(callbackName){
+        delete privateCallbacks[callbackName];
+        delete masterCallbacks[callbackName];
     }
 
     // Register a private callback, called when a JSONP response calls a global, master callback
     function registerCallback(callbackName, callback){
-        var callbacks = privateCallbacks[callbackName] || createCallbackCollection(callbackName);
+        var callbacks = privateCallbacks[callbackName] || createCollection(callbackName);
 
         callbacks.push(callback);
         return callback;
     }
 
-    function shiftCallback(callbackName){
-        var callbacks = privateCallbacks[callbackName];
-        return callbacks && callbacks.shift();
-    }
-
     /*
         NOTE: older IE's don't support `onerror` events when <script> elements fail to load; hence the callback may never fire with the error object, and the callback may not be removed from the container.
     */
-    function generateErrorHandler(callbackName, errorCallback){
-        return function(url){
-            // Remove the callback that led to the failed request
-            shiftCallback(callbackName);
-
+    function generateScriptCallback(callbackName, errorCallback){
+        return function(success, url){
             // Free up memory by deleting container
-            removeCallbackCollectionIfEmpty(callbackName);
+            removeCollection(callbackName);
 
-            if (errorCallback){
+            if (!success && errorCallback){
                 // Call the error callback with an error object
                 errorCallback({
                     error: 'JSONP failed',
@@ -168,34 +137,32 @@
 
     // Load a script into a <script> element
     // Modified from https://github.com/premasagar/cmd.js/tree/master/lib/getscript.js
-    function getscript(url, settings){
+    function getscript(url, callback, charset){
         var head = document.head || document.getElementsByTagName('head')[0],
             script = document.createElement('script'),
             active = true;
             
-        function cleanup(){
+        function cleanup(success){
             // Remove circular references to prevent memory leaks in IE
             active = script.onload = script.onreadystatechange = script.onerror = null;
             
             // Remove script element
             head.removeChild(script);
+
+            callback(success === true, url);
         }
 
         script.type = 'text/javascript';
-        script.charset = settings.charset || 'utf-8';
+        script.charset = charset || 'utf-8';
         script.src = url;
         script.onload = script.onreadystatechange = function(){
             var state = this.readyState;
             if (active && (!state || state === 'complete' || state === 'loaded')){
-                cleanup();
+                cleanup(true);
             }
         };
         // NOTE: IE8 and below don't fire error events
-        script.onerror = function(){
-            cleanup();
-            settings.error(url);
-        };
-
+        script.onerror = cleanup;
         head.appendChild(script);
     }
 
@@ -210,15 +177,14 @@
                 http://example.com?apicallback=mycallback
             - charset: (most likely you'll never need this) the value `charset` attribute to be added to the script that loads the JSONP. The remote API server should set the correct charset in its headers. Where it does not, the default value of `utf-8` is used. Where UTF-8 is not the desired charset, you can provide your own here.
     */
-    function jasonpeewee(url, params, callback, settings){
+    function jasonpeewee(url, params, successCallback, settings){
         var callbackParameter = 'callback',
-            scriptSettings = {},
-            errorCallback, callbackName;
+            charset, loadCallback, errorCallback, callbackName;
 
         // If `params` has not been passed
         if (typeof params === 'function'){
-            settings = callback;
-            callback = params;
+            settings = successCallback;
+            successCallback = params;
             params = null;
         }
 
@@ -231,21 +197,17 @@
             }
 
             // Error handler - called if the script fails to load
-            if (settings.error){
-                errorCallback = settings.error;
-            }
+            errorCallback = settings['error'];
 
-            // Set charset for script loading
-            if (settings.charset){
-                scriptSettings.charset = settings.charset;
-            }
+            // charset for script loading
+            charset = settings['charset'];
         }
 
         // Check if URL already contains a query string
         url += url.indexOf('?') === -1 ? '?' : '&';
 
         // Generate query string from settings
-        url += params ? encodeURLQueryString(params, true) + '&' : '';
+        url += params ? encodeAndSortQueryString(params) + '&' : '';
 
         // Create callbackName from the URL (including params)
         callbackName = makeJSCompatibleName(url);
@@ -257,13 +219,13 @@
         // if no cache, make JSONP request
         // Or trigger event, to allow third-party integration of caching
 
-        registerCallback(callbackName, callback);
+        registerCallback(callbackName, successCallback);
 
-        // Error handler to cleanup objects in memory, and call callback if set
-        scriptSettings.error = generateErrorHandler(callbackName, errorCallback);
+        // Error handler to cleanup objects in memory, and call optional callback
+        loadCallback = generateScriptCallback(callbackName, errorCallback);
 
         // Load the script
-        getscript(url, scriptSettings);
+        getscript(url, loadCallback, charset);
 
         return url;
     }
@@ -280,9 +242,6 @@
     */
     window[callbacksName] = masterCallbacks;
     jasonpeewee['path'] = callbacksName;
-
-    // Add useful methods
-    jasonpeewee['encodeURLQueryString'] = encodeURLQueryString;
 
 
     /////
